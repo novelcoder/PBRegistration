0e6cdac70006bb07015edc7d7452c035108e9741
jamesgreenwood <james@fickledragon.com>
2023-04-20 11:07:45 -0500

    add PartnerEmail functionality

---
+++ RegistrationTables/PartnerEmail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace RegistrationTables
+{
+    public class PartnerEmail
+    {
+        public string CombinedName { get; set; } = string.Empty;
+        public string Division { get; set; } = string.Empty;
+        public string PlayerDue { get; set; } = string.Empty;
+        public string PartnerDue { get; set; } = string.Empty;
+        public string Emails { get; set; } = string.Empty;
+        public string PlayerPaid { get; set; } = string.Empty;
+        public string PartnerPaid { get; set; } = string.Empty;
+	}
+}
+
---
+++ RegistrationTables/PartnerEmailReader.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+namespace RegistrationTables
+{
+	public class PartnerEmailReader
+    {
+        private Spreadsheet _spreadsheet = new Spreadsheet();
+
+        public PartnerEmailReader()
+        {
+            _spreadsheet = new Spreadsheet();
+            _spreadsheet.ConnectGoogle();
+        }
+
+        public List<PartnerEmail> ReadSpreadsheet()
+        {
+            var result = new List<PartnerEmail>();
+
+            string range = "PartnerEmail Save!A1:Y200";
+            var data = _spreadsheet.SheetsService.Spreadsheets.Values.Get(Spreadsheet.MDMUploadSheetId, range).Execute();
+
+            for (int iii = 0; iii < data.Values.Count; iii++)
+            {
+                var cols = data.Values[iii];
+                var emailRow = new PartnerEmail
+                {
+                    CombinedName = cols[0].ToString() ?? string.Empty,
+                    Division = cols[1].ToString() ?? string.Empty,
+                    PlayerDue = cols[2].ToString() ?? string.Empty,
+                    PartnerDue = cols[3].ToString() ?? string.Empty,
+                    Emails  = cols[4].ToString() ?? string.Empty
+                };
+
+                if (cols.Count() > 5)
+                    emailRow.PlayerPaid = cols[5].ToString() ?? string.Empty;
+                if (cols.Count() > 6)
+                    emailRow.PartnerPaid = cols[6].ToString() ?? string.Empty;
+                
+                result.Add(emailRow);
+            }
+
+            return result;
+        }
+    }
+}
+
