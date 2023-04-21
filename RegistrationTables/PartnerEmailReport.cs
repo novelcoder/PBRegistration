72682b9578f0b5ebaf88735c682c49c05c16ae77
jamesgreenwood <james@fickledragon.com>
2023-04-21 13:03:03 -0500

    add emails to partner email

--- RegistrationTables/PartnerEmailReport.cs
+++ RegistrationTables/PartnerEmailReport.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Globalization;
+using System.Numerics;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
+using Microsoft.Win32;
+using static System.Net.WebRequestMethods;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace RegistrationTables
@@ -20,7 +25,7 @@ namespace RegistrationTables
 
             var added = new List<string>();
 
-            result.Add(new List<object>() { "Player / Partner", "Division", "Player Due", "Partner Due", "Emails", "Player Paid", "Partner Paid", "Congrats EML" });
+            result.Add(new List<object>() { "Player / Partner", "Division", "Player Due", "Partner Due", "Emails", "Player Paid", "Partner Paid", "Congrats EML", "Need Money EML", "Need Data EML" });
             foreach (var person in people)
             {
                 foreach (var evt in person.Events)
@@ -62,6 +67,9 @@ namespace RegistrationTables
                                 colData.Add(partnerPaid);
                                 colData.Add(personPaid);
                                 colData.Add(SuccessMessage(salutation, div, partnerDue, personDue ));
+                                colData.Add(NeedMoneyMessage(salutation, div, partner.Name, PaidMsg(partner, partnerPaid),
+                                                                              person.Name, PaidMsg(person, personPaid)));
+                                colData.Add(NeedDataMessage(salutation, div));
                             }
                             else 
                             {
@@ -73,6 +81,9 @@ namespace RegistrationTables
                                 colData.Add(personPaid);
                                 colData.Add(partnerPaid);
                                 colData.Add(SuccessMessage(salutation, div, partnerDue, personDue));
+                                colData.Add(NeedMoneyMessage(salutation, div, person.Name, PaidMsg(person, personPaid)
+                                                                            , partner.Name, PaidMsg(partner, partnerPaid)));
+                                colData.Add(NeedDataMessage(salutation, div));
                             }
 
                             if (! FindPartnerEmail(savedEmails, person.Name, partner.Name))
@@ -82,8 +93,49 @@ namespace RegistrationTables
                     }
                 }
             }
+
+            // put in generic message about missing registration
+            colData = new List<object>();
+            for (int ii = 0; ii < 7; ii++)
+                colData.Add(string.Empty);
+            var sb = new StringBuilder();
+            sb.Append("Hi -\n\n");
+            sb.Append("I am so happy to see that you paid for the spots in the tournament.\n\n");
+            sb.Append("Would you please complete the registration, so that we can get you into the correct skills divisions and checked in as one of the teams!\n\n");
+            sb.Append("Here is the link to register:  https://geni.us/MDM-Reg\n\n");
+            sb.Append("We look forward to seeing you there!!!\n\n");
+            sb.Append("Cindy");
+            colData.Add(sb.ToString());
+            result.Add(colData);
+
             return result;
         }
+
+        private static string PaidMsg(Person person, string personPaid)
+        {
+            var sb = new StringBuilder();
+            var due = Event.Due(person.Events);
+            int amtDue = 0;
+            int amtPaid = 0;
+            string displayEvents = Event.DisplayList(person.Events);
+
+            Int32.TryParse(due, NumberStyles.Currency, CultureInfo.CurrentCulture, out amtDue);
+            Int32.TryParse(personPaid, NumberStyles.Currency, CultureInfo.CurrentCulture, out amtPaid);
+
+            if (amtDue <= amtPaid)
+                sb.Append($"I have you paid in full for your {person.Events.Count()} events ({displayEvents}).");
+            else if (amtPaid == 0)
+                sb.Append($"We have not received payment from you for events ({displayEvents}) total {due}. Please, submit payment as soon as possible.");
+            else
+            {
+                sb.Append($"We have received partial payment of ${amtPaid} ");
+                sb.Append($"and show {person.Events.Count()} events ({displayEvents}) ");
+                sb.Append($"leaving ${amtDue - amtPaid} unpaid. Please, submit payment as soon as possible.");
+            }
+
+            return sb.ToString();
+        }
+            
 
         private static object SuccessMessage(string salutation, string div, string partnerDue, string personDue)
         {
@@ -92,6 +144,34 @@ namespace RegistrationTables
             sb.Append($"I have you registered for {div} and all payments have been received.\n\n");
             sb.Append($"We look forward to seeing you!\n\n");
             sb.Append($"Have a great night!\n\n");
+            sb.Append("Cindy");
+            return sb.ToString();
+        }
+
+        private static object NeedMoneyMessage(string salutation, string div, string personName, string personPaidMsg, string partnerName, string partnerPaidMsg)
+        {
+            var sb = new StringBuilder();
+            sb.Append(salutation);
+            sb.Append($"I have you registered for {div}.\n\n");
+            sb.Append($"The first event is $40 and 2nd events are $20.\n\n");
+            sb.Append($"{personName} - {personPaidMsg}\n\n");
+            sb.Append($"{partnerName} - {partnerPaidMsg}\n\n");
+            sb.Append("Please see the links below for payment options.\n\n");
+            sb.Append("Venmo: https://venmo.com/pickleball-lincoln\n");
+            sb.Append("Paypal Email: PickleballLincoln @gmail.com\n");
+            sb.Append("Paypal Link: https://paypal.com/paypalme/pickleballLincoln\n\n");
+            sb.Append($"Please let me know if you have any questions.\n\n");
+            sb.Append("Cindy");
+            return sb.ToString();
+        }
+
+        private static object NeedDataMessage(string salutation, string div)
+        {
+            var sb = new StringBuilder();
+            sb.Append(salutation);
+            sb.Append($"I show your registration for {div}.\n\n");
+            sb.Append("Unfortunately, some of your information is missing and we cannot complete your registration. Please reply to this email with the following information.\n\n\n");
+            sb.Append($"Please let me know if you have any questions.\n\n");
             sb.Append("Cindy");
             return sb.ToString();
         }
